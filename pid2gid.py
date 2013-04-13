e34f57887f8bc34a31610f4663f9d44716947e3d
shhan <shhan@shhan.local>
2013-04-12 20:41:58 -0400

    update pid2gid

--- pid2gid.py
+++ pid2gid.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python 
-
 import sys
 import csv
 
@@ -12,9 +11,15 @@ pid_gid_file --- a pid gid mapping file. (CSV format)
 """
 
 def pid2gid(fileOne,pid_gid_file):
-    data = csv.reader(fileOne,delimiter=',')
-    pid_gid = csv.reader(pid_gid_file,delimiter=",")
+    pg = {}
+    data = []
+   
+    #load the probe ID to Gene ID
+    f = csv.reader(pid_gid_file,delimiter=",",quotechar='\"')
+    for row in f:
+        print row
     
+    #data = csv.reader(fileOne,delimiter=',')
 
-    
-    
+if __name__ == "__main__":
+    pid2gid(sys.argv[0],sys.argv[1])
