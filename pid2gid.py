9cbaed784e1c294f50d21c1a3cdc890431fc1b95
shhan <shhan@shhan.local>
2013-04-12 21:12:42 -0400

    finish pid2gid.py

--- pid2gid.py
+++ pid2gid.py
@@ -15,11 +15,26 @@ def pid2gid(fileOne,pid_gid_file):
     data = []
    
     #load the probe ID to Gene ID
-    f = csv.reader(pid_gid_file,delimiter=",",quotechar='\"')
-    for row in f:
-        print row
-    
-    #data = csv.reader(fileOne,delimiter=',')
+    with open(pid_gid_file,'rb') as p:
+        f = csv.reader(p,delimiter=",",quotechar='\"')
+        for row in f:
+            pg[row[1]] = row[2]
+
+    """ load data """
+    with open(fileOne,'rb') as q:
+        f = csv.reader(q,delimiter=",")
+        for row in f:
+            data.append(row)
+
+    """ mapping """
+    for item in data:
+        if pg.has_key(item[0]):
+            item[0] = pg[item[0]]
+
+    with open("mapped.csv",'wb') as r:
+        wf = csv.writer(r,dialect='excel')
+        for i in data:
+            wf.writerow(i)
 
 if __name__ == "__main__":
-    pid2gid(sys.argv[0],sys.argv[1])
+    pid2gid(str(sys.argv[1]),str(sys.argv[2]))
